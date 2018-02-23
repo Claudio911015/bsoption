35675841e4232d8f259cadc0bf346f22bb035dd1
Claudio911015 <claudiocp_2@hotmail.com>
2018-02-22 21:02:33 -0600

    Changes on variable names

--- R/Tests.R
+++ R/Tests.R
@@ -6,17 +6,17 @@ source("./R/vols.R")
 
 load("./data/opt_chain.rda")
 
-sabrVol <- calibrate(opt_chain,as.Date("2017-07-11"),model="sabr")
-quadVol <- calibrate(opt_chain,as.Date("2017-07-11"))
+sabrVol_ <- calibrate(opt_chain,as.Date("2017-07-11"),model="sabr")
+quadVol_ <- calibrate(opt_chain,as.Date("2017-07-11"))
 k <- seq(90,110,0.5)
-vols1 <- getVol(sabrVol,100,k)
-vols2 <- getVol(quadVol,100,k)
+vols1 <- getVol(sabrVol_,100,k)
+vols2 <- getVol(quadVol_,100,k)
 plot(k,vols1,type="n")
 lines(k,vols1,col="red")
 lines(k,vols2,col="blue")
 
 
-voldist <- impliedDistribution(sabrVol)
+voldist <- impliedDistribution(sabrVol_)
 plot(voldist$k,voldist$p,type="n")
 lines(voldist$k,voldist$p,col="blue")
 distributionPricer(100,101,voldist,type="call")
